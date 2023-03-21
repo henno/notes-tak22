838ecf19c84c42e92a9177d1dd9592a4c72edff0
Henno Täht <henno.taht@gmail.com>
2023-03-21 15:37:56 +0200

    As a user, I can log in to and out of my account so that I can securely access my information and ensure my privacy [Delivers #184536950]

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT || 3000
+const {v4: uuidv4} = require('uuid');
 
 // Add Swagger UI
 const swaggerUi = require('swagger-ui-express');
@@ -15,8 +16,23 @@ app.use(express.static('public'))
 app.use(express.json())
 
 const users = [
-    {id: 1, email: 'admin', password: 'admin'}
+    {id: 1, email: 'admin', password: '$2b$10$0EfA6fMFRDVQWzU0WR1dmelPA7.qSp7ZYJAgneGsy2ikQltX2Duey'} // KollneKollne
 ]
+
+let sessions = [
+    // {id: '123', userId: 1}
+]
+
+function tryToParseJson(jsonString) {
+    try {
+        var o = JSON.parse(jsonString);
+        if (o && typeof o === "object") {
+            return o;
+        }
+    } catch (e) {
+    }
+    return false;
+}
 
 app.post('/users', async (req, res) => {
 
@@ -36,7 +52,11 @@ app.post('/users', async (req, res) => {
         }
         console.log('Email verified')
     } catch (error) {
-        return res.status(400).send('Invalid email: ' + error.code)
+        const errorObject = tryToParseJson(error)
+        if (errorObject && errorObject.info) {
+            return res.status(400).send('Invalid email: ' + errorObject.info)
+        }
+        return res.status(400).send('Invalid email: ' + error)
     }
 
     // Hash password
@@ -57,6 +77,78 @@ app.post('/users', async (req, res) => {
 
 })
 
+// POST /sessions
+app.post('/sessions', async (req, res) => {
+
+    // Validate email and password
+    if (!req.body.email || !req.body.password) return res.status(400).send('Email and password are required')
+
+    // Find user in database
+    const user = users.find(user => user.email === req.body.email)
+    if (!user) return res.status(404).send('User not found')
+
+    // Compare password
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+
+            // Create session
+            const session = {id: uuidv4(), userId: user.id}
+
+            // Add session to sessions array
+            sessions.push(session)
+
+            // Send session to client
+            res.status(201).send(session)
+
+        } else {
+            res.status(401).send('Invalid password')
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal server error')
+    }
+
+})
+
+function authorizeRequest(req, res, next) {
+    // Check that there is an authorization header
+    if (!req.headers.authorization) return res.status(401).send('Missing authorization header')
+
+    // Check that the authorization header is in the correct format
+    const authorizationHeader = req.headers.authorization.split(' ')
+    if (authorizationHeader.length !== 2 || authorizationHeader[0] !== 'Bearer') return res.status(400).send('Invalid authorization header')
+
+    // Get sessionId from authorization header
+    const sessionId = authorizationHeader[1]
+
+    // Find session in sessions array
+    const session = sessions.find(session => session.id === sessionId)
+    if (!session) return res.status(401).send('Invalid session')
+
+    // Check that the user exists
+    const user = users.find(user => user.id === session.userId)
+    if (!user) return res.status(401).send('Invalid session')
+
+    // Add user to request object
+    req.user = user
+
+    // Add session to request object
+    req.session = session
+
+    // Call next middleware
+    next()
+
+}
+
+app.delete('/sessions', authorizeRequest, (req, res) => {
+
+    // Remove session from sessions array
+    sessions = sessions.filter(session => session.id !== req.session.id)
+
+    res.status(204).end()
+
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
@@ -66,7 +158,7 @@ function verifyEmail(email) {
         verifier.verify(email, (err, info) => {
             console.log(err, info);
             if (err) {
-                reject(JSON.stringify(err));
+                reject(JSON.stringify(info));
             } else {
                 resolve(info);
             }
